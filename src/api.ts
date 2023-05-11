2aabf06b3855462982460cfc512542c5c73a6a30
jeroen <jpjagt@pm.me>
2023-05-11 19:13:56 +0800

    initial commit

---
+++ src/api.ts
@@ -0,0 +1,13 @@
+import apiData from './apiData'
+
+export const useFetchTabletypeInfosQuery = () => {
+  return { data: apiData['tabletypeInfosOfOrg'], isFetching: false }
+}
+
+export const useFetchDoctypeInfosQuery = () => {
+  return { data: apiData['doctypeInfosOfOrg'], isFetching: false }
+}
+
+export const useFetchDocumentInfosQuery = () => {
+  return { data: apiData['documentInfosOfOrg'], isFetching: false }
+}
---
+++ src/apiData.ts
@@ -0,0 +1,343 @@
+export default {
+  accountInfos: [
+    {
+      id: '1971400000123456544',
+      slug: 'frail-motion',
+    },
+    {
+      id: '19714000001509045',
+      slug: '22-ghosts',
+    },
+  ],
+  doctypeInfosOfOrg: [
+    {
+      doctype: 'buma-stemra_csv_0',
+      label: '[CD | 1tt | detail]',
+      updated_at: '1683802428126',
+    },
+    {
+      doctype: 'buma-stemra_csv_1',
+      label: '[CD | 1tt | summary csv]',
+      updated_at: '1683812428126',
+    },
+    {
+      doctype: 'buma-stemra_csv_2',
+      label: '[CD | 1tt]',
+      updated_at: '1683822428126',
+    },
+    {
+      doctype: 'buma-stemra_csv_3',
+      label: '[CD | 1tt]',
+      updated_at: '1683832428126',
+    },
+    {
+      doctype: 'buma-stemra_csv_4',
+      label: '[CD | 1tt | financial detail csv]',
+      updated_at: '1683842428126',
+    },
+    {
+      doctype: 'buma-stemra_csv_5',
+      label: '[CD | 1tt | registration summary]',
+      updated_at: '1683852428126',
+    },
+    {
+      doctype: 'buma-stemra_pdf_0',
+      label: '[CD | 1tt]',
+      updated_at: '1683862428126',
+    },
+    {
+      doctype: 'buma-stemra_xlsx_0',
+      label: '[NODATA | 0tt]',
+      updated_at: '1683872428126',
+    },
+    {
+      doctype: 'buma-stemra_xlsx_1',
+      label: '[CD | 1tt]',
+      updated_at: '1683882428126',
+    },
+  ],
+  tabletypeInfosOfOrg: [
+    {
+      tabletype: 'buma-stemra_0',
+      label: '[25 cols]',
+      updated_at: '1683802420000',
+    },
+    {
+      tabletype: 'buma-stemra_4',
+      label: '[financial-detail | 38 cols]',
+      updated_at: '1683812420000',
+    },
+    {
+      tabletype: 'buma-stemra_5',
+      label: '[financial-detail | 38 cols | detail export]',
+      updated_at: '1683822420000',
+    },
+    {
+      tabletype: 'buma-stemra_6',
+      label: '[overview | 7 cols | summary csv - complete]',
+      updated_at: '1683832420000',
+    },
+    {
+      tabletype: 'buma-stemra_7',
+      label: '[registration | 5 cols]',
+      updated_at: '1683842420000',
+    },
+    {
+      tabletype: 'buma-stemra_8',
+      label: '[registration | 5 cols]',
+      updated_at: '1683852420000',
+    },
+    {
+      tabletype: 'buma-stemra_9',
+      label: '[overview | 5 cols | summary]',
+      updated_at: '1683862420000',
+    },
+    {
+      tabletype: 'buma-stemra_10',
+      label: '[registration | 8 cols]',
+      updated_at: '1683872420000',
+    },
+    {
+      tabletype: 'buma-stemra_11',
+      label: '[financial-detail | 38 cols]',
+      updated_at: '1683882420000',
+    },
+    {
+      tabletype: 'buma-stemra_12',
+      label: '[registration | 8 cols]',
+      updated_at: '1683892420000',
+    },
+  ],
+  documentInfosOfOrg: [
+    {
+      id: 1712,
+      account_id: '1971400000123456544',
+      affiliation_id: '19714000001509029',
+      created_at: '1683802420000',
+      doctype_id: 12,
+      ss_file_id: '85c91037-6cbd-4808-a206-eca8049ece72',
+      fhash: 'e94eb11f3e143ba4edda0c2600bb40eb',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/frail-motion_1971400000123456544_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/85c91037-6cbd-4808-a206-eca8049ece72/generate-distributions-export_1-1-2019-31-12-2019.csv',
+      doctype: 'buma-stemra_csv_2',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_5': {
+          '0': 10059,
+        },
+      },
+    },
+    {
+      id: 1709,
+      account_id: '1971400000123456544',
+      affiliation_id: '19714000001509029',
+      created_at: '1683812420000',
+      doctype_id: 11,
+      ss_file_id: '104eb4ba-4092-436a-8b72-547d4ffa3843',
+      fhash: '67b8e371d5d5e031e867d2b565ba4587',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/frail-motion_1971400000123456544_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/104eb4ba-4092-436a-8b72-547d4ffa3843/statements-1298621-I-002124872-4-RC0000492-till-RC0000474.csv',
+      doctype: 'buma-stemra_csv_1',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_6': {
+          '0': 10056,
+        },
+      },
+    },
+    {
+      id: 1708,
+      account_id: '1971400000123456544',
+      affiliation_id: '19714000001509029',
+      created_at: '1683822420000',
+      doctype_id: 12,
+      ss_file_id: '7117cdd1-c792-4758-9326-01eb1217a19f',
+      fhash: 'c0c7eb5546405cd48b7863af80dc3cb3',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/frail-motion_1971400000123456544_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/7117cdd1-c792-4758-9326-01eb1217a19f/generate-distributions-export_1-1-2020-31-12-2020.csv',
+      doctype: 'buma-stemra_csv_2',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_5': {
+          '0': 10055,
+        },
+      },
+    },
+    {
+      id: 1711,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '1683832420000',
+      doctype_id: 11,
+      ss_file_id: '176997f8-8c9b-4395-b9c1-899732416a2f',
+      fhash: '52a969413a341a8a5e7919233ceb1645',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/176997f8-8c9b-4395-b9c1-899732416a2f/statements-1298621-I-002124872-4-RC0000372-till-RC0000350.csv',
+      doctype: 'buma-stemra_csv_1',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_6': {
+          '0': 10058,
+        },
+      },
+    },
+    {
+      id: 1717,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '1683842420000',
+      doctype_id: 11,
+      ss_file_id: '63fb9497-faba-4fd2-8f4f-89fec557b6cc',
+      fhash: '1c5f10fa7bc39c48cdd60ee44fd65d7a',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/63fb9497-faba-4fd2-8f4f-89fec557b6cc/statements-1298621-I-002124872-4-RC0000469-till-RC0000442.csv',
+      doctype: 'buma-stemra_csv_1',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_6': {
+          '0': 10064,
+        },
+      },
+    },
+    {
+      id: 1713,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '1683852420000',
+      doctype_id: 12,
+      ss_file_id: '223d3f0d-3210-453c-89b4-79c1698072c6',
+      fhash: 'ade87716a4414e32a022a34cc06a7bd7',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/223d3f0d-3210-453c-89b4-79c1698072c6/generate-distributions-export_1-1-2017-31-12-2017.csv',
+      doctype: 'buma-stemra_csv_2',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_5': {
+          '0': 10060,
+        },
+      },
+    },
+    {
+      id: 1715,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '1683862420000',
+      doctype_id: 12,
+      ss_file_id: '3cb0653a-112f-4d02-930d-8086726f53a2',
+      fhash: 'bf6c082f43c6ce55ec655a72609dfe41',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/3cb0653a-112f-4d02-930d-8086726f53a2/generate-distributions-export_1-1-2022-31-12-2022.csv',
+      doctype: 'buma-stemra_csv_2',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_5': {
+          '0': 10062,
+        },
+      },
+    },
+    {
+      id: 1706,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '1683872420000',
+      doctype_id: 11,
+      ss_file_id: '7b9a4a21-6489-4677-9bf8-305b6bc3fb94',
+      fhash: '127c7e175971c57dcc319f402346d3d0',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/7b9a4a21-6489-4677-9bf8-305b6bc3fb94/statements-1298621-I-002124872-4-RC0000320-till-RC0000298.csv',
+      doctype: 'buma-stemra_csv_1',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_6': {
+          '0': 10053,
+        },
+      },
+    },
+    {
+      id: 1716,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '1683882420000',
+      doctype_id: 12,
+      ss_file_id: '661974fd-1ab7-4217-90a1-79ed48868312',
+      fhash: '2f8e0713fb3ae2e9792bcdcdd7de8fd3',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/661974fd-1ab7-4217-90a1-79ed48868312/generate-distributions-export_1-1-2021-31-12-2021.csv',
+      doctype: 'buma-stemra_csv_2',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_5': {
+          '0': 10063,
+        },
+      },
+    },
+    {
+      id: 1719,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '1683892420000',
+      doctype_id: 11,
+      ss_file_id: 'c9b9c7fe-3336-4abf-946b-b24ece54eb2e',
+      fhash: 'ee22bb7abf0d058e85b53f09161b3568',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/c9b9c7fe-3336-4abf-946b-b24ece54eb2e/statements-1298621-I-002124872-4-RC0000404-till-RC0000380.csv',
+      doctype: 'buma-stemra_csv_1',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_6': {
+          '0': 10066,
+        },
+      },
+    },
+    {
+      id: 1707,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '16838102420000',
+      doctype_id: 12,
+      ss_file_id: '9bf2eda8-c9d7-4fa0-9b8c-4411fce5c758',
+      fhash: '37e986a89d0501524b82d84d3c4d66ff',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/9bf2eda8-c9d7-4fa0-9b8c-4411fce5c758/generate-distributions-export_1-1-2018-31-12-2018.csv',
+      doctype: 'buma-stemra_csv_2',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_5': {
+          '0': 10054,
+        },
+      },
+    },
+    {
+      id: 1714,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '16838112420000',
+      doctype_id: 11,
+      ss_file_id: 'c8ca70d7-cb0e-41a1-a5cc-32b22854a77a',
+      fhash: 'a3951b25a808e707338ab775e47bb041',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/c8ca70d7-cb0e-41a1-a5cc-32b22854a77a/statements-1298621-I-002124872-4-RC0000439-till-RC0000412.csv',
+      doctype: 'buma-stemra_csv_1',
+      tabletype2tnum2document_table_id: {
+        'buma-stemra_6': {
+          '0': 10061,
+        },
+      },
+    },
+    {
+      id: 1718,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '16838122420000',
+      doctype_id: null,
+      ss_file_id: 'c1dcb080-76c5-4517-b3a2-5c185299d4a2',
+      fhash: '8a38f3d274c147e31b7a9f518c1a2448',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/c1dcb080-76c5-4517-b3a2-5c185299d4a2/generate-distributions-export_1-1-2016-31-12-2016.csv',
+      doctype: null,
+      tabletype2tnum2document_table_id: {},
+    },
+    {
+      id: 1710,
+      account_id: '19714000001509045',
+      affiliation_id: '19714000001509029',
+      created_at: '16838132420000',
+      doctype_id: null,
+      ss_file_id: 'ed472569-20d4-4bf8-80ae-4eed5fd4fd56',
+      fhash: '915e5ac8057f40e3ea4e74e33148078e',
+      fpath:
+        '/srv/dev/sendsafely/symlinked/22-ghosts_19714000001509045_prod/active_tjeerd-bomhof-22ghosts_songwriter_buma-stemra_19714000001126039_19714000001055215_19714000001055049_prod/ed472569-20d4-4bf8-80ae-4eed5fd4fd56/statements-1298621-I-002124872-4-RC0000347-till-RC0000325.csv',
+      doctype: null,
+      tabletype2tnum2document_table_id: {},
+    },
+  ],
+}
